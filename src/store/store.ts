299f743b893c9b0be46d1ddc86800c0be0e26348
Jose David <lujan9495@gmail.com>
2024-11-15 09:21:46 -0500

    Escribiendo en el Estado de Zustand

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 import { create } from "zustand";
 import { DraftPatient, Patient } from "../types";
+import {v4 as uuidv4} from 'uuid'
 
 //Creamos el type de los pacientes
 type PatientStore = {
@@ -8,10 +9,19 @@ type PatientStore = {
   addPatient: (data: DraftPatient) => void // Toma data, que son los datos extraídos por el usuario del formulario 
 }
 
+const createPatient = (patient : DraftPatient) : Patient => {
+  return {...patient, id: uuidv4()}
+}
+
 // Creación del store con "create"
-export const usePatientStore = create<PatientStore>(() => ({
+export const usePatientStore = create<PatientStore>((set) => ({
+  
   patients: [], // State
+  
   addPatient: (data) => { // Función para pasarle el paciente 
-    console.log(data)
+    const newPatient = createPatient(data)
+    set((state) => ({
+      patients: [...state.patients, newPatient]
+    }))
   } 
 }))
