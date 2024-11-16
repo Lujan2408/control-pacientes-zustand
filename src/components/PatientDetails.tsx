63b61fd059377e26b034b95cb860b8ecfb3dbb17
Jose David <lujan9495@gmail.com>
2024-11-15 20:45:24 -0500

    Mostrando la información del paciente desde un nuevo componente

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 import { Patient } from "../types"
+import PatientDetailItem from "./PatientDetailItem"
 
 type PatientDetailsProps = {
   patient: Patient
@@ -7,8 +8,13 @@ type PatientDetailsProps = {
 
 export default function PatientDetails({patient} : PatientDetailsProps) {
   return (
-    <div>
-      {patient.name}
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-xl rounded-xl">
+      <PatientDetailItem label="ID" data={patient.id} />
+      <PatientDetailItem label="Nombre" data={patient.name} />
+      <PatientDetailItem label="Propietario" data={patient.caretaker} />
+      <PatientDetailItem label="Correo" data={patient.email} />
+      <PatientDetailItem label="Fecha de Ingreso" data={patient.date.toString()} />
+      <PatientDetailItem label="Síntomas" data={patient.symptoms} />
     </div>
-  )
+  );
 }
